9f92bfd70aa960382f0f1e9ef2c37f4df6066831
shaiiiikh-github <shabbir23rd@gmail.com>
2025-05-25 12:34:15 +0530

    `Added service worker registration and PWA metadata to index.html and notepad.js`

--- notepad.js
+++ notepad.js
@@ -46,3 +46,11 @@ function deleteNote(id) {
 function loadNotes() {
     displayNotes();
 }
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(reg => console.log('Service Worker registered', reg))
+      .catch(err => console.error('Service Worker failed', err));
+  }
+
+
---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'pixel-notepad-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/notepad.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
